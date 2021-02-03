7a7a0dcf7ef98de1cdea0dbf06e4a0bbf2b765d5
Moonage-Ross-Volkova <depressedwitchuwur@gmail.com>
2021-02-03 19:02:30 +0000

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+char;
+int;
+long int;
+long long int;
+float;
+
+printf("Size of a char: %ld bytes\n", sizeof(char));
+printf("Size of an int: %ld bytes\n", sizeof(int));
+printf("Size of a long: %ld bytes\n", sizeof(long int));
+printf("Size of a long long int: %ld byte\n", sizeof(long long int));
+printf("Size of a float: %ld bytes\n", sizeof(float));
+return (0);
+}
