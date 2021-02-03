c589d9074c39c00eadde12127ef5db56fbf6f75f
Moonage-Ross-Volkova <depressedwitchuwur@gmail.com>
2021-02-03 19:04:23 +0000

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@ long int;
 long long int;
 float;
 
-printf("Size of a char: %ld bytes\n", sizeof(char));
-printf("Size of an int: %ld bytes\n", sizeof(int));
-printf("Size of a long: %ld bytes\n", sizeof(long int));
-printf("Size of a long long int: %ld byte\n", sizeof(long long int));
-printf("Size of a float: %ld bytes\n", sizeof(float));
+printf("Size of a char: %lu bytes\n", sizeof(char));
+printf("Size of an int: %lu bytes\n", sizeof(int));
+printf("Size of a long: %lu bytes\n", sizeof(long int));
+printf("Size of a long long int: %lu byte\n", sizeof(long long int));
+printf("Size of a float: %lu bytes\n", sizeof(float));
 return (0);
 }
